912a7f42ee7a6d62a3a50075db1e21d9a2140b44
yussan <main.yussan@gmail.com>
2026-02-12 04:19:11 +0700

    fix(mongodb-config): add pooling setiup

--- configs/setup.go
+++ configs/setup.go
@@ -13,17 +13,22 @@ import (
 
 func ConnectDB() *mongo.Client {
 	clientOptions := options.Client().ApplyURI(EnvMongoURI())
-	
+
 	// Configure TLS
 	clientOptions.SetTLSConfig(&tls.Config{
 		InsecureSkipVerify: true,
 	})
-	
+
 	// Set timeouts
 	clientOptions.SetServerSelectionTimeout(30 * time.Second)
 	clientOptions.SetConnectTimeout(30 * time.Second)
 	clientOptions.SetSocketTimeout(30 * time.Second)
-	
+
+	// Configure connection pool
+	clientOptions.SetMaxPoolSize(100)                  // Maximum number of connections in the pool
+	clientOptions.SetMinPoolSize(5)                    // Minimum number of connections in the pool
+	clientOptions.SetMaxConnIdleTime(30 * time.Minute) // Maximum time a connection can be idle before being closed
+
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		log.Fatal(err)
@@ -31,7 +36,7 @@ func ConnectDB() *mongo.Client {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	
+
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -46,10 +51,10 @@ func ConnectDB() *mongo.Client {
 	return client
 }
 
-//Client instance
+// Client instance
 var DB *mongo.Client = ConnectDB()
 
-//getting database collections
+// getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
 	collection := client.Database(EnvMongoDB()).Collection(collectionName)
 	return collection
